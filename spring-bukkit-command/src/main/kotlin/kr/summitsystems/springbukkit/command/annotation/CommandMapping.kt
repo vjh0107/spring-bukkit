8f4aa2e474bf9a7d14715f2a261684721923f743
junhyung <park@junhyung.kr>
2024-01-27 01:27:33 +0900

    Change order of @CommandMapping's parameters

--- spring-bukkit-command/src/main/kotlin/kr/summitsystems/springbukkit/command/annotation/CommandMapping.kt
+++ spring-bukkit-command/src/main/kotlin/kr/summitsystems/springbukkit/command/annotation/CommandMapping.kt
@@ -2,4 +2,4 @@ package kr.summitsystems.springbukkit.command.annotation
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class CommandMapping(val command: String = "", val path: String = "")
+annotation class CommandMapping(val path: String = "", val command: String = "")
